9d4587d13704489cbbcc315142df0e7c17333138
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-04-25 20:08:18 +0200

    Add activity Fridge

---
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge.kt
@@ -0,0 +1,12 @@
+package com.example.inteligentnalodowka_mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fridge : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fridge)
+    }
+}
--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.inteligentnalodowka_mobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonFridge.setOnClickListener{
+            goToWindowFridge()
+        }
+
+    }
+
+    private fun goToWindowFridge(){
+        val activityGoToFridge = Intent(applicationContext, Fridge::class.java)
+        startActivity(activityGoToFridge)
     }
 }
