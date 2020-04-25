71e9d0195ae635b279ec903c80be41a8b6e9f379
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-04-25 23:17:39 +0200

    Add recyclerView to Recipes Activity

---
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/ShowAllRecipesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inteligentnalodowka_mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowAllRecipesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_all_recipes)
+    }
+}
