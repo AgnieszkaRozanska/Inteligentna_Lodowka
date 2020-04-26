417fb4d1fbd83dcc826fb4bf39be4529b43cba04
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-04-26 14:01:00 +0200

    Implementation of the Product Details Activity

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/FridgeActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/FridgeActivity.kt
@@ -1,13 +1,19 @@
 package com.example.inteligentnalodowka_mobileapp.Fridge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.inteligentnalodowka_mobileapp.R
+import kotlinx.android.synthetic.main.activity_fridge.*
 
 class FridgeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fridge)
+        button.setOnClickListener {
+            val activityGoToFridge = Intent(applicationContext, ProductDetailsActivity::class.java)
+            startActivity(activityGoToFridge)
+        }
     }
 }
---
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/ProductDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.inteligentnalodowka_mobileapp.Fridge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inteligentnalodowka_mobileapp.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
