a62f92c970e7ee1c0963d28d6f6632ffbdfc44b0
Dżesika Szymańska <dzesszy738@student.polsl.pl>
2020-04-27 19:39:06 +0200

    Tabela z bazą produktów
    Utworzenie tabeli dla produktów pochodzących z testowej bazy danych, poprawa nazw zmiennych.

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/DataBaseHandler.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/DataBaseHandler.kt
@@ -6,80 +6,98 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 
-const  val DATABASE_NAME = "Lodowka.db"
+const  val DATABASE_NAME = "Fridge.db"
 
 //TABELA PRODUKTÓW UŻYTKOWNIKA W LODÓWCE
 
-const val PRODUKTY_TABLE_NAME = "Produkty"
-const val ID_PRODUKTU = "ID"
-const val NAZWA_PRODUKTU = "Nazwa"
-const val DATA_PRZYDATNOSCI = "Data_przydatnosci"
-const val ILOSC = "Ilosc"
-const val TYP = "Typ"
+const val PRODUCTS_TABLE_NAME = "Products"
+const val ID_PRODUCT = "ID"
+const val NAME_PRODUCT = "Name"
+const val EXPIRATION_DATE = "Expiration_date"
+const val QUANTITY = "Quantity"
+const val TYPE = "Type"
 
 //TABELA PRZEPISÓW
 
-const val PRZEPISY_TABLE_NAME = "Przepisy"
-const val ID_PRZEPISU = "ID"
-const val NAZWA_PRZEPISU = "Nazwa"
-const val CZAS_WYKONANIA = "Czas_wykonania"
-const val OPIS = "Opis"
+const val RECIPES_TABLE_NAME = "Recipes"
+const val ID_RECIPE = "ID"
+const val NAME_RECIPE = "Name"
+const val EXECUTION_TIME = "Execution_time"
+const val DESCRIPTION = "Description"
 
 //TABELA PRODUTKY DO PRZEPISOW
-const val PRODUKTY_DO_PRZEPISOW_TABLE_NAME = "Produkty_do_przepisow"
-const val ID_PRZEPISU2 = "Id_przepisu"
-const val ID_PRODUKTU2 = "Id_produktu"
-const val ILOSC_PRODUKTU = "Ilosc"
+const val PRODUCTS_TO_RECIPES_TABLE_NAME = "Products_to_recipes"
+const val ID_RECIPE2 = "Id_recipe"
+const val ID_PRODUCT2 = "Id_product"
+const val PRODUCT_QUANTITY = "Quantity"
+
+//BAZA PRODUKTOW - TABELA WSTEPNA
+const val PRODUCTS_DATABASE_TABLE_NAME = "Products_Database"
+const val ID_PRODUCT_DATABASE = "ID"
+const val EAN_QR_CODE = "Code"
+const val NAME_PRODUCT_DATABASE = "Name"
+const val TYPE_PRODUCT_DATABASE = "Type"
 
 
 //TABELA PRODUKTY
-const val SQL_CREATE_TABLE_PRODUKTY = ("CREATE TABLE IF NOT EXISTS "  + PRODUKTY_TABLE_NAME +" (" +
-        ID_PRODUKTU + " TEXT PRIMARY KEY," +
-        NAZWA_PRODUKTU + " TEXT NOT NULL," +
-        DATA_PRZYDATNOSCI + " DATETIME," +
-        ILOSC + " TEXT" +
-        TYP + " TEXT)")
+const val SQL_CREATE_TABLE_PRODUCTS = ("CREATE TABLE IF NOT EXISTS "  + PRODUCTS_TABLE_NAME +" (" +
+        ID_PRODUCT + " TEXT PRIMARY KEY," +
+        NAME_PRODUCT + " TEXT NOT NULL," +
+        EXPIRATION_DATE + " DATETIME," +
+        QUANTITY + " TEXT" +
+        TYPE + " TEXT)")
 
 //TABELA PRZEPISY
-const val SQL_CREATE_TABLE_PRZEPISY= ("CREATE TABLE IF NOT EXISTS "  + PRZEPISY_TABLE_NAME +" (" +
-        ID_PRZEPISU + " TEXT PRIMARY KEY," +
-        NAZWA_PRZEPISU + " TEXT NOT NULL," +
-        CZAS_WYKONANIA + " TEXT," +
-        OPIS + " TEXT)")
+const val SQL_CREATE_TABLE_RECIPES= ("CREATE TABLE IF NOT EXISTS "  + RECIPES_TABLE_NAME +" (" +
+        ID_RECIPE + " TEXT PRIMARY KEY," +
+        NAME_RECIPE+ " TEXT NOT NULL," +
+        EXECUTION_TIME + " TEXT," +
+        DESCRIPTION + " TEXT)")
 
 //TABELA PRODUKTY DO PRZEPISOW
-const val SQL_CREATE_TABLE_PRODUKTY_DO_PRZEPISOW= ("CREATE TABLE IF NOT EXISTS "  + PRODUKTY_DO_PRZEPISOW_TABLE_NAME +" (" +
-        ID_PRZEPISU2 + " TEXT NOT NULL," +
+const val SQL_CREATE_TABLE_PRODUCTS_TO_RECIPES= ("CREATE TABLE IF NOT EXISTS "  + PRODUCTS_TO_RECIPES_TABLE_NAME +" (" +
+        ID_RECIPE2 + " TEXT NOT NULL," +
         "FOREIGN KEY(Id_przepisu) REFERENCES Przepisy(ID," +
-        ID_PRODUKTU2 + " TEXT NOT NULL," +
+        ID_PRODUCT2 + " TEXT NOT NULL," +
         "FOREIGN KEY(Id_produktu) REFERENCES Produkty(ID)," +
-        ILOSC_PRODUKTU + " TEXT)")
+        PRODUCT_QUANTITY + " TEXT)")
+
+//BAZA PRODUKTOW
+const val SQL_CREATE_TABLE_PRODUCTS_DATABASE = ("CREATE TABLE IF NOT EXISTS "  + PRODUCTS_DATABASE_TABLE_NAME +" (" +
+        ID_PRODUCT_DATABASE + " TEXT PRIMARY KEY," +
+        EAN_QR_CODE + " TEXT NOT NULL," +
+        NAME_PRODUCT_DATABASE + " TEXT NOT NULL," +
+        TYPE_PRODUCT_DATABASE + " TEXT)")
 
 //TABALA PRODUKTY
-const val SQL_DELETE_TABLE_PRODUKTY = "DROP TABLE IF EXISTS $PRODUKTY_TABLE_NAME"
+const val SQL_DELETE_TABLE_PRODUCTS= "DROP TABLE IF EXISTS $PRODUCTS_TABLE_NAME"
 
 
 //TABELA PRZEPISY
-const val SQL_DELETE_TABLE_PRZEPISY = "DROP TABLE IF EXISTS $PRZEPISY_TABLE_NAME"
+const val SQL_DELETE_TABLE_RECIPES = "DROP TABLE IF EXISTS $RECIPES_TABLE_NAME"
 
 //TABELA PRODUKTY DO PRZEPISOW
-const val SQL_DELETE_TABLE_PRODUKTY_DO_PRZEPISOW = "DROP TABLE IF EXISTS $PRODUKTY_DO_PRZEPISOW_TABLE_NAME"
+const val SQL_DELETE_TABLE_PRODUCTS_TO_RECIPES = "DROP TABLE IF EXISTS $PRODUCTS_TO_RECIPES_TABLE_NAME"
+
+//BAZA PRODUKTOW
+const val SQL_DELETE_TABLE_PRODUCTS_DATABASE = "DROP TABLE IF EXISTS $PRODUCTS_DATABASE_TABLE_NAME"
 
 class SQLConector(context: Context): SQLiteOpenHelper(context,
     DATABASE_NAME, null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
         // tutaj tworzysz tabele:
-        db.execSQL(SQL_CREATE_TABLE_PRODUKTY)
-        db.execSQL(SQL_CREATE_TABLE_PRZEPISY)
-        db.execSQL(SQL_CREATE_TABLE_PRODUKTY_DO_PRZEPISOW)
+        db.execSQL(SQL_CREATE_TABLE_PRODUCTS)
+        db.execSQL(SQL_CREATE_TABLE_RECIPES)
+        db.execSQL(SQL_CREATE_TABLE_PRODUCTS_TO_RECIPES)
+        db.execSQL(SQL_CREATE_TABLE_PRODUCTS_DATABASE)
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        // tutaj usuwasz tabele
-        db.execSQL(SQL_DELETE_TABLE_PRODUKTY)
-        db.execSQL(SQL_DELETE_TABLE_PRZEPISY)
-        db.execSQL(SQL_DELETE_TABLE_PRODUKTY_DO_PRZEPISOW)
+        db.execSQL(SQL_DELETE_TABLE_PRODUCTS)
+        db.execSQL(SQL_DELETE_TABLE_RECIPES)
+        db.execSQL(SQL_DELETE_TABLE_PRODUCTS_TO_RECIPES)
+        db.execSQL(SQL_DELETE_TABLE_PRODUCTS_DATABASE)
         onCreate(db)
     }
 }
