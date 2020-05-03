43c7ab75fe9b9f83dadd5b8845ad10df43170e9a
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-05-03 17:44:03 +0200

    Add class MultiDexApp

---
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Scan/MultiDexApp.kt
@@ -0,0 +1,10 @@
+package com.example.inteligentnalodowka_mobileapp.Scan
+
+import androidx.multidex.MultiDexApplication
+
+class MultiDexApp : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
