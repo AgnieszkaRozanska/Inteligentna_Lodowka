18f7b47eafeed1ad2f9235ce36d7cf40441d1648
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-05-03 18:19:31 +0200

    Add AlertDialog
    Add AlertDialog if there is no product in the database

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/Scan/ScanPrroductsActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Scan/ScanPrroductsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.inteligentnalodowka_mobileapp.Scan
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.inteligentnalodowka_mobileapp.R
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
@@ -42,4 +44,13 @@ class ScanPrroductsActivity : AppCompatActivity() {
         }
     }
 
+
+
+    private fun alertDialogNoProductInDatabase() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.AlertDialogTitle))
+        builder.setMessage(getString(R.string.AlertDialogMessageNoProductInDatabase))
+        builder.setPositiveButton(getString(R.string.Back)) { dialog: DialogInterface, which: Int -> }
+        builder.show()
+    }
 }
