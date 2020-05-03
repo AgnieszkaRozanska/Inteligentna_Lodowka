5e7f96a695b87edd8970709b5314c2659b5652e8
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-05-03 19:21:59 +0200

    Add spinner and set   visibility of items
    Add a spinner to the product type
    hide / show some elements

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/Scan/ScanPrroductsActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Scan/ScanPrroductsActivity.kt
@@ -4,6 +4,9 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.inteligentnalodowka_mobileapp.R
@@ -14,6 +17,8 @@ import kotlinx.android.synthetic.main.activity_scan_prroducts.*
 class ScanPrroductsActivity : AppCompatActivity() {
 
     var eanCode = ""
+    var flagScanned = false
+    var list_of_types= arrayOf("Wybierz typ", "Warzywa", "Owoce", "Nabiał", "Słodycze", "Przekąski", "Mięso", "Ryby", "Produkty zbożowe", "Inne")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,10 @@ class ScanPrroductsActivity : AppCompatActivity() {
             }
         }
 
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_types)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTypeOfProduct!!.setAdapter(aa)
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -35,7 +44,8 @@ class ScanPrroductsActivity : AppCompatActivity() {
 
             if(result.contents != null){
                 eanCode = result.contents
-                textViewValue.text = eanCode
+                //textViewValue.text = eanCode
+                checkEanCode(eanCode)
             } else {
                 Toast.makeText(this, getString(R.string.scanfailed), Toast.LENGTH_LONG).show()
             }
@@ -44,13 +54,32 @@ class ScanPrroductsActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkEanCode(eanCode : String){
+
+        // tu bedzie kod sprawdzający, czy w api mamy produkt czy też nie
+        // jeżeli go nei będzie mamy alert dialog
+
+        alertDialogNoProductInDatabase()
+
+    }
 
 
     private fun alertDialogNoProductInDatabase() {
+        textViewNameProduct.setVisibility(View.VISIBLE)
+        textViewNumberOfPackagesInfo.setVisibility(View.VISIBLE)
+        textViewTypeOfProductInfo.setVisibility(View.VISIBLE)
+        textViewExpirationDate.setVisibility(View.VISIBLE)
+        textViewDate.setVisibility(View.VISIBLE)
+        spinnerTypeOfProduct.setVisibility(View.VISIBLE)
+        editTextNumberOfPackages.setVisibility(View.VISIBLE)
+        textViewInformed.setVisibility(View.INVISIBLE)
         val builder = AlertDialog.Builder(this)
         builder.setTitle(getString(R.string.AlertDialogTitle))
         builder.setMessage(getString(R.string.AlertDialogMessageNoProductInDatabase))
         builder.setPositiveButton(getString(R.string.Back)) { dialog: DialogInterface, which: Int -> }
         builder.show()
     }
+
+
+
 }
