a2705afe2341eeaf97ab7f96daa641086b8e5b79
Dżesika Szymańska <dzesszy738@student.polsl.pl>
2020-06-14 15:21:47 +0200

    Add filter method
    Not working

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/ShowAllProducts/FridgeActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/ShowAllProducts/FridgeActivity.kt
@@ -3,22 +3,51 @@ package com.example.inteligentnalodowka_mobileapp.Fridge.ShowAllProducts
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inteligentnalodowka_mobileapp.DataBaseHandler
 import com.example.inteligentnalodowka_mobileapp.MainActivity
 import com.example.inteligentnalodowka_mobileapp.R
-import com.example.inteligentnalodowka_mobileapp.Recipies.ConcreteRecipeActivity
 import kotlinx.android.synthetic.main.activity_fridge.*
 
 class FridgeActivity : AppCompatActivity() {
+    lateinit var adapter: ShowAllProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fridge)
 
         setTextIfListIsEmpty()
+
+        editTextSearchProducts.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                adapter.filter.filter(newText)
+                return false
+            }
+        })
+
+
+        getListOfProducts()
+
     }
+
+
+    private fun getListOfProducts() {
+        val dbHelper = DataBaseHandler(this)
+        dbHelper.writableDatabase
+        val  productList = dbHelper.getAllProducts()
+
+        adapter = ShowAllProductsAdapter(this, productList)
+
+    }
+
 
     override fun onBackPressed() {
         val intentOnBackPress = Intent(applicationContext, MainActivity::class.java)
@@ -34,16 +63,21 @@ class FridgeActivity : AppCompatActivity() {
 
         recyclerViewAllProducts.layoutManager = LinearLayoutManager(this)
         recyclerViewAllProducts.adapter =  ShowAllProductsAdapter(this, productList)
+
     }
 
-    private fun  setTextIfListIsEmpty(){
+    private fun  setTextIfListIsEmpty() {
         val sqlConector = DataBaseHandler(this)
-        val  productsList=sqlConector.getAllProducts()
+        val productsList = sqlConector.getAllProducts()
 
-        if(productsList.isNullOrEmpty()) {
+        if (productsList.isNullOrEmpty()) {
             textViewInformationAboutLackOfProducts.text = "Brak produktów"
             textViewInformationAboutLackOfProducts.visibility = TextView.VISIBLE
-        }
-        else textViewInformationAboutLackOfProducts.visibility = TextView.INVISIBLE
+        } else textViewInformationAboutLackOfProducts.visibility = TextView.INVISIBLE
+
+
     }
 }
+
+
+
--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/ShowAllProducts/ShowAllProductsAdapter.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/Fridge/ShowAllProducts/ShowAllProductsAdapter.kt
@@ -6,35 +6,85 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.inteligentnalodowka_mobileapp.Fridge.ProductDetailsActivity
 import com.example.inteligentnalodowka_mobileapp.Product
 import com.example.inteligentnalodowka_mobileapp.R
 import kotlinx.android.synthetic.main.activity_card_view_all_products.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ShowAllProductsAdapter(context: Context, var productsList: ArrayList<Product>): RecyclerView.Adapter<MyViewHolder>() {
+class ShowAllProductsAdapter(context: FridgeActivity, var productsList: ArrayList<Product>): RecyclerView.Adapter<MyViewHolder>(),
+    Filterable {
+
+    var productsFilterList = ArrayList<Product>()
+
+    init {
+        productsFilterList = productsList
+    }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(viewGroup.context)
-        val cardViewProduct = layoutInflater.inflate(R.layout.activity_card_view_all_products, viewGroup, false)
+        val cardViewProduct =
+            layoutInflater.inflate(R.layout.activity_card_view_all_products, viewGroup, false)
+
+
+
+
         return MyViewHolder(cardViewProduct)
     }
 
     override fun getItemCount(): Int {
-        return productsList.count()
+        return productsFilterList.count()
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    productsFilterList = productsList
+                } else {
+                    val resultList = ArrayList<Product>()
+                    for (row in productsList) {
+
+                        if (row.nameProduct.toLowerCase(Locale.ROOT)
+                                .contains(charSearch.toLowerCase(Locale.ROOT))
+                        ) {
+                            resultList.add(row)
+                        }
+                    }
+                    productsFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = productsFilterList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                productsFilterList = results?.values as ArrayList<Product>
+                notifyDataSetChanged()
+            }
+
+        }
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-        val productName_cardView=productsList[holder.adapterPosition].nameProduct
-        val typeProduct_cardView = productsList[holder.adapterPosition].type
-        val countProduct_cardView = productsList[holder.adapterPosition].quantity
+
+
+        val productName_cardView = productsFilterList[holder.adapterPosition].nameProduct
+        val typeProduct_cardView = productsFilterList[holder.adapterPosition].type
+        val countProduct_cardView = productsFilterList[holder.adapterPosition].quantity
 
         holder.productName.text = productName_cardView
         holder.productType.text = typeProduct_cardView
-        holder.productCount.text = "Liczba opakowań: "+countProduct_cardView
+        holder.productCount.text = "Liczba opakowań: " + countProduct_cardView
 
         if (position % 2 == 1) {
             holder.cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"))
@@ -43,15 +93,15 @@ class ShowAllProductsAdapter(context: Context, var productsList: ArrayList<Produ
         }
 
         val cardViewProduct = holder.view.cardView_product
-        val context:Context = holder.view.context
+        val context: Context = holder.view.context
 
         cardViewProduct.setOnClickListener {
             val intentEdit = Intent(context, ProductDetailsActivity::class.java)
-            val priductID = productsList[holder.adapterPosition].id
-            val priductName =productsList[holder.adapterPosition].nameProduct
-            val productPurchaseDate = productsList[holder.adapterPosition].purchaseDate
-            val productExpirationDate=productsList[holder.adapterPosition].expirationDate
-            val productQuantity = productsList[holder.adapterPosition].quantity
+            val priductID = productsFilterList[holder.adapterPosition].id
+            val priductName = productsFilterList[holder.adapterPosition].nameProduct
+            val productPurchaseDate = productsFilterList[holder.adapterPosition].purchaseDate
+            val productExpirationDate = productsFilterList[holder.adapterPosition].expirationDate
+            val productQuantity = productsFilterList[holder.adapterPosition].quantity
 
             intentEdit.putExtra("id", priductID)
             intentEdit.putExtra("name", priductName)
@@ -63,8 +113,8 @@ class ShowAllProductsAdapter(context: Context, var productsList: ArrayList<Produ
         }
 
     }
-}
 
+}
 class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     var productName : TextView = view.findViewById(R.id.ProductName_cardView)
     var productType : TextView = view.findViewById(R.id.Type_cardView)
