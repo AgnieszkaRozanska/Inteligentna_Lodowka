bd5eb9e6fccbf246ab3a274210358e96dd7672f5
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-07-09 11:08:39 +0200

    Add class ShoppingProduct

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/ShoppingList/ShoppingProduct.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/ShoppingList/ShoppingProduct.kt
@@ -1,4 +1,17 @@
 package com.example.inteligentnalodowka_mobileapp.ShoppingList
 
 class ShoppingProduct {
+    var id : String
+    var nameShoppingProduct : String
+    var howMuch : String
+    var type : String
+    var ifBuy : Int
+
+    constructor(id : String, nameShoppingProduct : String, howMuch: String, type : String, ifBuy : Int){
+        this.id = id
+        this.nameShoppingProduct = nameShoppingProduct
+        this.howMuch = howMuch
+        this.type = type
+        this.ifBuy = ifBuy
+    }
 }
