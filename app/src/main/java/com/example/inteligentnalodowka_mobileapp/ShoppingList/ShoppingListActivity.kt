4ca72e84b5d2c2947e9aaee09920eeb3f41e684a
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-07-14 18:19:06 +0200

    Final version of the shopping list functionality

--- app/src/main/java/com/example/inteligentnalodowka_mobileapp/ShoppingList/ShoppingListActivity.kt
+++ app/src/main/java/com/example/inteligentnalodowka_mobileapp/ShoppingList/ShoppingListActivity.kt
@@ -1,11 +1,14 @@
 package com.example.inteligentnalodowka_mobileapp.ShoppingList
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inteligentnalodowka_mobileapp.DataBaseHandler
@@ -14,6 +17,7 @@ import com.example.inteligentnalodowka_mobileapp.R
 import kotlinx.android.synthetic.main.activity_shopping_list.*
 import kotlinx.android.synthetic.main.custom_alertdialog_add_shopping_product.*
 import kotlinx.android.synthetic.main.custom_alertdialog_add_shopping_product.view.*
+import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -21,19 +25,21 @@ class ShoppingListActivity : AppCompatActivity() {
 
     var list_of_types= arrayOf("Wybierz typ produktu", "Sztuki", "Opakowania", "Butelki", "Inne", "g", "dag", "kg","ml","l")
     var list_of_kind= arrayOf("Wybierz rodzaj", "Warzywa", "Owoce", "Nabiał", "Słodycze", "Przekąski", "Mięso", "Ryby", "Produkty zbożowe", "Napoje", "Inne")
-
+    var flagIfExists = false
+    var nameShoppingItem = ""
+    var idShoppingItem = ""
+    var howMuchShoppingItem = ""
+    var typeShoppingItem = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_list)
 
-
-
+        setTextIfListIsEmpty()
 
         AddShoppingProduct.setOnClickListener {
             AlertDialogAddShoppingProduct()
         }
-
     }
 
     override fun onBackPressed() {
@@ -51,6 +57,8 @@ class ShoppingListActivity : AppCompatActivity() {
 
         recyclerViewShoppingList.layoutManager = LinearLayoutManager(this)
         recyclerViewShoppingList.adapter =  ShoppingListAdapter(this, productList)
+
+        setTextIfListIsEmpty()
     }
 
 
@@ -74,35 +82,50 @@ class ShoppingListActivity : AppCompatActivity() {
         adapterKinds.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         mDialogView.editTextTyp!!.setAdapter(adapterKinds)
 
-
-
         mDialogView.Save.setOnClickListener {
-            mAlertDialog.dismiss()
+            try {
+                var nameShoppingProduct = mDialogView.EditText_NameShoppingProduct.text.toString()
 
-            val dbHelper = DataBaseHandler(this)
-            val idProduct = UUID.randomUUID().toString()
-            var nameShoppingProduct = mDialogView.EditText_NameShoppingProduct.text.toString()
+                var kind = mDialogView.spinner_kindAmount.getSelectedItem().toString()
+                var howMuchInt = mDialogView.editText_Amount.text.toString().toInt()
+                var howMuch_result = customizeTheText(kind, howMuchInt)
 
-            var kind = mDialogView.spinner_kindAmount.getSelectedItem().toString()
-            var howMuchInt = mDialogView.editText_Amount.text.toString().toInt()
-            var howMuch_result = customizeTheText(kind, howMuchInt)
+                var howMuch = howMuch_result
+                var type = mDialogView.editTextTyp.getSelectedItem().toString()
+                val idProduct = UUID.randomUUID().toString()
 
-            var howMuch = howMuch_result
-            var type = mDialogView.editTextTyp.getSelectedItem().toString()
+                nameShoppingItem = nameShoppingProduct
+                idShoppingItem = idProduct
+                howMuchShoppingItem = howMuch
+                typeShoppingItem = type
 
-            var product = ShoppingProduct(
-                idProduct,
-                nameShoppingProduct,
-                howMuch,
-                type,
-                0
-            )
+                var ifCorrect = checkCorrectOfData(nameShoppingProduct, type, howMuch, kind)
+                var ifNotExist = ifNotExists(nameShoppingProduct, type,  howMuch_result)
 
-            val success = dbHelper.addShoppingProduct(product)
-            onResume()
+                if(ifCorrect && flagIfExists) {
+
+                    val dbHelper = DataBaseHandler(this)
 
 
+                    var product = ShoppingProduct(
+                        idProduct,
+                        nameShoppingProduct,
+                        howMuch,
+                        type,
+                        0
+                    )
+
+                    val success = dbHelper.addShoppingProduct(product)
+                    mAlertDialog.dismiss()
+                    onResume()
+
+                }
+            }
+            catch (e: Exception) {
+                Toast.makeText(this, "Uzupełnij wszytskie dane", Toast.LENGTH_LONG).show()
+            }
         }
+
         mDialogView.Back.setOnClickListener {
             mAlertDialog.dismiss()
         }
@@ -112,7 +135,6 @@ class ShoppingListActivity : AppCompatActivity() {
     fun customizeTheText(type : String, howMuch : Int) : String{
 
         var result = ""
-        //"Sztuki", "Opakowania", "Butelki", "Inne", "g", "dag", "kg","ml","l")
         if(type.equals("Sztuki")) result = "sztuk: " + howMuch.toString()
         else if(type.equals("Opakowania")) result = "opakowań: " + howMuch.toString()
         else if(type.equals("Butelki")) result = "butelek: " + howMuch.toString()
@@ -135,4 +157,101 @@ class ShoppingListActivity : AppCompatActivity() {
         return resultsList
     }
 
+    fun checkCorrectOfData(nameShoppingItem : String, type : String, howMuch: String, kind : String) : Boolean{
+        var result = true;
+
+        if(nameShoppingItem.isEmpty() || type.isEmpty() || type == "Wybierz rodzaj" || howMuch.isEmpty() || kind == "Wybierz typ produktu")
+        {
+            result = false
+            alertDialogLackOfData()
+        }
+        flagIfExists = result
+        return result
+    }
+
+    private fun alertDialogLackOfData() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.AlertDialogTitleLackOfData))
+        builder.setMessage(getString(R.string.AlertDialogMessageShoppingProductLackOfData))
+        builder.setPositiveButton(getString(R.string.Back)) { dialog: DialogInterface, which: Int -> }
+        builder.show()
+    }
+
+    fun ifNotExists(nameShoppingItem : String, type : String,  kind : String){
+        val dbHelper = DataBaseHandler(this)
+        val  productList = dbHelper.getShoppingList()
+
+       var kind_from_app = kind.split(' ')
+        for (i in productList) {
+            var kind_from_database = i.howMuch.split(' ')
+            if(i.nameShoppingProduct.equals(nameShoppingItem) && i.type.equals(type) && !kind_from_app[0].equals(kind_from_database[0])){
+                alertDialogShoppingItemMaybeExists()
+                //alertDialogShoppingItemExists()
+                flagIfExists = false
+            }
+            if(i.nameShoppingProduct.equals(nameShoppingItem) && i.type.equals(type) && kind_from_app[0].equals(kind_from_database[0])){
+                //
+                flagIfExists = false
+                alertDialogShoppingItemExists()
+            }
+        }
+    }
+
+
+    private fun alertDialogShoppingItemExists() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.alertDialogAttention))
+        builder.setMessage(getString(R.string.alertDialogShoppingItemExistsMessage))
+        builder.setPositiveButton(getString(R.string.Back)) { dialog: DialogInterface, which: Int -> }
+        builder.show()
+    }
+
+    private fun alertDialogShoppingItemMaybeExists(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.alertDialogAttention))
+        builder.setMessage(getString(R.string.alertDialogShoppingItemMaybeExistsMessage))
+        builder.setPositiveButton(getString(R.string.AlertDialogYes)) { dialog: DialogInterface, which: Int ->
+            flagIfExists = true
+            addShoppingProduct_EvenThoughItMaybeExist(idShoppingItem, nameShoppingItem, howMuchShoppingItem, typeShoppingItem)
+
+        }
+       builder.setNegativeButton(getString(R.string.AlertDialogNo)){ dialog: DialogInterface, which: Int ->
+           flagIfExists = false
+
+       }
+        builder.show()
+    }
+
+
+    fun addShoppingProduct_EvenThoughItMaybeExist(idProduct : String, nameShoppingProduct : String, howMuch : String, type : String){
+        val dbHelper = DataBaseHandler(this)
+
+
+        var product = ShoppingProduct(
+            idProduct,
+            nameShoppingProduct,
+            howMuch,
+            type,
+            0
+        )
+
+        val success = dbHelper.addShoppingProduct(product)
+
+        val intentOnBackPress = Intent(applicationContext, ShoppingListActivity::class.java)
+        startActivity(intentOnBackPress)
+
+        onResume()
+    }
+
+
+    private fun  setTextIfListIsEmpty(){
+        val sqlConector = DataBaseHandler(this)
+        val  productsList=sqlConector.getShoppingList()
+
+        if(productsList.isNullOrEmpty()) {
+            textView_InformAboutLackOfShoppingList.visibility = TextView.VISIBLE
+        }
+        else textView_InformAboutLackOfShoppingList.visibility = TextView.INVISIBLE
+    }
 }
